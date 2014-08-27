ce72a578f7360a15f580b9443050eaac3c2840e0
Mark Fee <git@markfee.com>
2014-08-27 07:44:09 +0100

    balances calculating correctly

--- app/database/seeds/TransactionsTableSeeder.php
+++ app/database/seeds/TransactionsTableSeeder.php
@@ -10,22 +10,6 @@ class TransactionsTableSeeder extends Seeder {
 
     foreach($records as $record)
     {
-/*
-TRANSID: "124",
-ACCOUNTID: "2",
-TOACCOUNTID: "-1",
-PAYEEID: "106",
-TRANSCODE: "Withdrawal",
-TRANSAMOUNT: "40",
-STATUS: "R",
-TRANSACTIONNUMBER: "",
-NOTES: "",
-CATEGID: "16",
-SUBCATEGID: "47",
-TRANSDATE: "2012-02-04",
-FOLLOWUPID: "-1",
-TOTRANSAMOUNT: "40"
-*/
       $amount = $record->TRANSAMOUNT;
       $credit_account = null;
       $debit_account = null;
@@ -40,7 +24,6 @@ TOTRANSAMOUNT: "40"
       }
 
       Transaction::create([
-//        "id"                => $record->TRANSID,
         "date"              => $record->TRANSDATE,
         "amount"            => $amount * 100,
         "account_id"        => $record->ACCOUNTID,
@@ -51,7 +34,6 @@ TOTRANSAMOUNT: "40"
       ]);
       if ($record->TOACCOUNTID != "-1") { // transfer
         Transaction::create([
-//          "id"                => $record->TRANSID,
           "date"              => $record->TRANSDATE,
           "amount"            => $amount * -100,
           "account_id"        => $record->TOACCOUNTID,
@@ -60,11 +42,9 @@ TOTRANSAMOUNT: "40"
           "category_id"       => $categoryId == "-1"      ? null : $categoryId,
           "notes"             => $record->NOTES,
         ]);
-
       }
-
-
 		}
+    DB::unprepared('call refresh_balances();');
 	}
 
 }

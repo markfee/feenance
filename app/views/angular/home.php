8313de7356e993a97b2335b80782433ad7529577
Mark Fee <git@markfee.com>
2014-08-29 20:29:26 +0100

    transfer source and destination showing via directive

--- app/views/angular/home.php
+++ app/views/angular/home.php
@@ -11,6 +11,12 @@
     </span>
 </script>
 
+<script type="text/ng-template" id="transaction.html">
+  <span ng-show="transaction.id">
+    {{direction}} {{account.title}}
+  </span>
+</script>
+
 
 
 <div data-ng-controller="TransactionsController" ng-show="items">
@@ -20,8 +26,8 @@
     <tr>                            <th>Date    </th> <th>Credit</th> <th>Debit</th>  <th>Balance</th>   </tr>
     <tr ng-repeat="item in items">
       <td>{{item.date | date}} </td>
-      <td><span ng-show="item.amount>0">{{item.amount  | currency: "£" }}   </span></td>
-      <td><span ng-show="item.amount<0">{{item.amount  | currency: "£" }}   </span></td>
+      <td><span ng-show="item.amount>0">{{item.amount  | currency: "£" }}   </span> <transaction source="item.source" />       </td>
+      <td><span ng-show="item.amount<0">{{item.amount  | currency: "£" }}   </span> <transaction destination="item.destination" /> </td>
       <td>{{item.balance | currency: "£" }}</td>
 <!--  <td><span ng-show="accountId==item.credit.account_id">{{item.credit.amount  | currency: "£" }}  <account direction="from" accountid="item.credit.transfer_id">  </account> </span></td>
       <td><span ng-show="accountId==item.debit.account_id">{{item.debit.amount  | currency: "£" }}    <account direction="to" accountid="item.credit.transfer_id">  </account> </span></td>-->

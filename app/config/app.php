a0177fef0d69fbeedbb599392a2448dd6d9d9c6c
Mark Fee <git@markfee.com>
2014-08-30 07:07:49 +0100

    added ide helper

--- app/config/app.php
+++ app/config/app.php
@@ -123,6 +123,7 @@ return array(
 		'Illuminate\Workbench\WorkbenchServiceProvider',
     'Way\Generators\GeneratorsServiceProvider',
     'Markfee\Responder\ResponderServiceProvider',
+    'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
 	),
 
 	/*

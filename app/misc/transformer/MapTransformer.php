f363f551c25ac90358f590342e127b7e042a84e7
Mark Fee <git@markfee.com>
2014-09-18 06:07:58 +0100

    create map working with transfer

--- app/misc/transformer/MapTransformer.php
+++ app/misc/transformer/MapTransformer.php
@@ -13,8 +13,16 @@ class MapTransformer extends Transformer {
 
   public static function transform($record) {
     return [
-      "id"                => (int)$record->id,
-      "name"              => $record->name,
+      "id"                => (int)  $record->id,
+      "account_id"        => (int)  $record->account_id,
+      "payee_id"          => (int)  $record->payee_id,
+      "category_id"       => (int)  $record->category_id,
+      "transfer_id"    => $record->transfer_id? (int)  $record->transfer_id: null,
+      "account"        => AccountsTransformer::transform($record->account),
+      "payee"          => PayeeTransformer::transform($record->payee),
+      "category"       => CategoryTransformer::transform($record->category),
+      "transfer"       => AccountsTransformer::transform($record->transfer),
+
       ];
   }
 }
--- app/models/Map.php
+++ app/models/Map.php
@@ -4,10 +4,16 @@ class Map extends \Eloquent {
 
 	// Add your validation rules here
 	public static $rules = [
-		// 'name' => 'required'
-	];
+      "payee_id"    => 'required'
+    , "account_id"  => 'required'
+    , "category_id" => 'required'
+    ];
 
 	// Don't forget to fill this array
-	protected $fillable = ["payee_id", "account_id", "category_id"];
+	protected $fillable = ["payee_id", "account_id", "category_id", "transfer_id"];
 
+  public function account()     { return $this->belongsTo("Account"); }
+  public function payee()       { return $this->belongsTo("Payee"); }
+  public function category()    { return $this->belongsTo("Category"); }
+  public function transfer()    { return $this->belongsTo("Account"); }
 }

641e69711d4143b089c69935120f813be0a6ff2a
Mark Fee <git@markfee.com>
2014-09-28 11:20:20 +0100

    added category to transactionTable; started category view

--- app/views/includes/header.blade.php
+++ app/views/includes/header.blade.php
@@ -1,5 +1,8 @@
 <nav class="navbar navbar-inverse">
   <div class="navbar-header">
     <a class="navbar-brand" href="{{ URL::to('/') }}">Feenance</a>
+    <a class="navbar-brand" href="{{ URL::to('/standing_orders') }}">Standing Orders</a>
+    <a class="navbar-brand" href="{{ URL::to('/import') }}">Import</a>
+    <a class="navbar-brand" href="{{ URL::to('/categories') }}">Category</a>
   </div>
 </nav>

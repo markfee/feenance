45d978c09a4132e5f18dc2f032fccb1ad3530a98
Mark Fee <git@markfee.com>
2014-11-15 09:19:11 +0000

    refactored standingOrderTransformer to include all fields

--- app/misc/transformer/StandingOrderTransformer.php
+++ app/misc/transformer/StandingOrderTransformer.php
@@ -19,25 +19,24 @@ class StandingOrderTransformer extends Transformer {
       "next_date"         => $record->next_date->toISO8601String(),
       "finish_date"       => $record->finish_date ? $record->finish_date->toISO8601String() : null,
       "increment"         => (int) $record->increment,
+      "unit_id"           => $record->unit_id,
       "unit"              => $record->unit,
       "frequency"         => $record->frequency,
-      "addition"         => $record->addition,
+      "addition"          => $record->addition,
       "days"              => $record->days,
       "exceptions"        => $record->exceptions,
       "amount"            => 0.01 * $record->amount,
-      "skip_to_bank_day"  => (boolean)$record->next_bank_day,
+      "next_bank_day"     => (boolean)$record->next_bank_day,
 
-      "account_id"        => $record->account_id,
-      "destination_id"        => $record->destination_account_id,
+      "account_id"              => $record->account_id              ? (int) $record->account_id : null,
+      "destination_account_id"  => $record->destination_account_id  ? (int) $record->destination_account_id : null,
 
-      "account"    => AccountsTransformer::transform($record->account),
-      "destination"     => AccountsTransformer::transform($record->destination),
+      "account"           => AccountsTransformer::transform($record->account),
+      "destination"       => AccountsTransformer::transform($record->destination),
 
-      "notes"             => $record->notes?:null ,
+      "notes"             => $record->notes?  : null ,
       "payee"             => $record->payee_id ?    PayeeTransformer::transform($record->payee) : null,
-//      "category"          => $record->category,
       "category"          => $record->category_id ? CategoryTransformer::transform($record->category) : null,
-
     ];
   }
 }

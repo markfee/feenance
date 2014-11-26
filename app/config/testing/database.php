49e1353aa72a7ec291a0a1affca9067b5368011a
Mark Fee <git@markfee.com>
2014-11-26 07:36:11 +0000

    simplified database config files

--- app/config/testing/database.php
+++ app/config/testing/database.php
@@ -8,25 +8,10 @@
 return array(
   'default' => 'sqlite',
   'connections' => array(
-    'mmex' => array(
-      'driver'   => 'sqlite',
-      'database' => '/home/mark/www/.mmex/mmexini.db3',
-      'prefix'   => '',
-    ),
     'sqlite' => array(
       'driver'   => 'sqlite',
       'database' => ':memory:',
       'prefix'   => ''
-    ),
-    'mysqltest' => array(
-      'driver'    => 'mysql',
-      'host'      => 'mysqltest',
-      'database'  => 'snodbert',
-      'username'  => 'snodbert_user',
-      'password'  => 'x7%99uIijsjweiLKJGjk3',
-      'charset'   => 'utf8',
-      'collation' => 'utf8_unicode_ci',
-      'prefix'    => '',
     ),
   )
 );

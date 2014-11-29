09835a1595e68b4bee6788ceeb682d7a31e9a383
Mark Fee <git@markfee.com>
2014-11-29 09:18:18 +0000

    migration to add primary key to transfers

---
+++ app/database/migrations/2014_11_29_084445_add_primary_key_to_transfers.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPrimaryKeyToTransfers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+    Schema::create('temp_transfers', function(Blueprint $table)
+    {
+      $table->engine = DB::connection()->getConfig("engine");
+      // These are source and destination transactions NOT acccounts.
+      // This table links two transactions together as a transfer.
+      $table->increments('id');
+      $table->integer('source')->unsigned();
+      $table->integer('destination')->unsigned();
+    });
+
+    DB::unprepared("INSERT temp_transfers (source, destination) SELECT source, destination FROM transfers;");
+
+    Schema::drop('transfers');
+
+    Schema::rename("temp_transfers", "transfers");
+
+    Schema::table('transfers', function(Blueprint $table)
+    {
+      $table->foreign('source')->references('id')->on('transactions')->onDelete('cascade')->unique();
+      $table->foreign('destination')->references('id')->on('transactions')->onDelete('cascade')->unique();
+    });
+
+
+
+  }
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+    // NO NEED TO DO ANYTHING
+	}
+
+}

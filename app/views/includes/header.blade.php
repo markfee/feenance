82de81f3303827abae7166afaa73cdc241224410
Mark Fee <git@markfee.com>
2015-01-04 09:37:12 +0000

    altered menu

--- app/views/includes/header.blade.php
+++ app/views/includes/header.blade.php
@@ -1,12 +1,16 @@
 <nav class="navbar navbar-inverse">
   <div class="navbar-header">
-    <a class="navbar-brand" href="{{ URL::to('/') }}">Feenance</a>
-    <a class="navbar-brand" href="{{ URL::to('/standing_orders') }}">Standing Orders</a>
-    <a class="navbar-brand" href="{{ URL::to('/transfers') }}">Transfers</a>
-    <a class="navbar-brand" href="{{ URL::to('/potential_transfers') }}">Potential Transfers</a>
-    <a class="navbar-brand" href="{{ URL::to('/import') }}">Import</a>
-    <a class="navbar-brand" href="{{ URL::to('/reports/categories') }}">Category</a>
+    <a class="navbar-brand" href="{{ URL::to('/') }}">                      Feenance</a>
+    <a class="navbar-brand" href="{{ URL::to('/') }}">                      All Transactions</a>
+    <a class="navbar-brand" href="{{ URL::to('/account_transactions') }}">  Account Transactions</a>
+    <a class="navbar-brand" href="{{ URL::to('/standing_orders') }}">       Standing Orders</a>
+    <a class="navbar-brand" href="{{ URL::to('/transfers') }}">             Transfers</a>
+    <a class="navbar-brand" href="{{ URL::to('/potential_transfers') }}">   Potential Transfers</a>
+    <a class="navbar-brand" href="{{ URL::to('/import') }}">                Import</a>
+    <a class="navbar-brand" href="{{ URL::to('/reports/categories') }}">    Category</a>
+
     <button ng-click="toggleDebug()">Debug</button>
     <button ng-click="clearCache()">Clear Cache</button>
+
   </div>
 </nav>

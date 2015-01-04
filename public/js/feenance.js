8f2ef3bf6682412a4203cf754c73982e28de5aea
Mark Fee <git@markfee.com>
2015-01-04 12:46:04 +0000

    added angular pagination to transactions view

--- public/js/feenance.js
+++ public/js/feenance.js
@@ -16,6 +16,16 @@ feenance.filter('isoDate', function() {
   };
 });
 
+//We already have a limitTo filter built-in to angular,
+//let's make a startFrom filter
+feenance.filter('startFrom', function() {
+  return function(input, start) {
+    start = +start; //parse to int
+    return input.slice(start);
+  }
+});
+
+
 feenance.controller('FeenanceController', function($scope, $templateCache) {
   $scope.debug = false;
 
--- public/js/transactions.js
+++ public/js/transactions.js
@@ -1,11 +1,21 @@
 feenance.factory('TransactionCollection', function(TransactionsApi, Collection) {
-    return new Collection(TransactionsApi, "<transactions...>");
+    return (new Collection(TransactionsApi, "<transactions...>")).fetchAll();
 });
 
 feenance.controller('TransactionsController', function($scope, TransactionCollection, CollectionSelection)
 {
     var collectionSelection = new CollectionSelection(TransactionCollection, $scope, "id");
 
+    $scope.currentPage = 00;
+    $scope.pageSize = 20;
+    $scope.numberOfPages=function(){
+        return Math.ceil($scope.boundCollection.length/$scope.pageSize);
+    }
+
+    $scope.filter = function(element)
+    {
+        return element.page <= 3;
+    }
 });
 
 feenance.directive('transactionsTable', function() {

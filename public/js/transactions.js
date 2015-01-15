7034de7b59fc20ea44914b02c894792afec5a4a0
Mark Fee <git@markfee.com>
2015-01-15 06:40:28 +0000

    transactions table fetches all transactions (for now - until pagination completed properly)

--- public/js/transactions.js
+++ public/js/transactions.js
@@ -6,16 +6,18 @@ feenance.controller('TransactionsController', function($scope, TransactionCollec
 {
     var collectionSelection = new CollectionSelection(TransactionCollection, $scope, "id");
 
-    $scope.currentPage = 00;
+    $scope.currentPage = 0;
     $scope.pageSize = 20;
-    $scope.numberOfPages=function(){
+    $scope.numberOfPages=function()
+    {
         return Math.ceil($scope.boundCollection.length/$scope.pageSize);
-    }
+    };
 
     $scope.filter = function(element)
     {
+        return true;
         return element.page <= 3;
-    }
+    };
 });
 
 feenance.directive('transactionsTable', function() {
@@ -24,7 +26,7 @@ feenance.directive('transactionsTable', function() {
         scope: {    },
         templateUrl: '/view/transaction_table.html'
         , link: function (scope) {
-
+            scope.pageSize = 30;
         }
         , controller: "TransactionsController"
     };

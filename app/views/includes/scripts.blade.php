0468206f29fdcd812f8f17d1b30707efdbbfccf2
Mark Fee <git@markfee.com>
2015-01-22 08:36:05 +0000

    set to include all of bootstrap for now with some overrides.

--- app/views/includes/scripts.blade.php
+++ app/views/includes/scripts.blade.php
@@ -1,10 +1,15 @@
+
+
 <!-- load bootstrap and jquery from a cdn -->
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+{{--<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">--}}
+
+<link rel="stylesheet" href="assets/stylesheets/frontend.css">
+
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
-{{ HTML::style('js/leaflet/leaflet.css') }}
-{{ HTML::script('js/leaflet/leaflet-src.js') }}
+{{--{{ HTML::style('js/leaflet/leaflet.css') }}--}}
+{{--{{ HTML::script('js/leaflet/leaflet-src.js') }}--}}
 
 <!-- load angular -->
 <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular.js"></script>
@@ -12,7 +17,6 @@
 <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-resource.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-cookies.js"></script>
 
-{{ HTML::style('style/feenance.css') }}
 <!--{{ HTML::script('js/angular/angular-leaflet-directive.min.js') }}-->
 {{ HTML::script('js/angular/ng-upload.js') }}
 {{ HTML::script('js/angular/ui-bootstrap-tpls-0.11.0.js') }}
@@ -32,3 +36,6 @@
 {{ HTML::script('js/potential_transfers.js') }}
 {{ HTML::script('js/transfers.js') }}
 {{ HTML::script('js/transactions.js') }}
+
+
+{{--{{ HTML::style('style/feenance.css') }}--}}

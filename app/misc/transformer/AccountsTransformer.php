5d738ce065d9929e64a4c380818bc9e1996d9848
Mark Fee <git@markfee.com>
2015-03-14 07:51:29 +0000

    whitespace

--- app/misc/transformer/AccountsTransformer.php
+++ app/misc/transformer/AccountsTransformer.php
@@ -1,4 +1,5 @@
 <?php namespace Feenance\Misc\Transformers;
+
 /**
  * Created by PhpStorm.
  * User: mark
@@ -10,25 +11,26 @@ use Markfee\Responder\Transformer;
 use Markfee\Responder\TransformerInterface;
 
 
-class AccountsTransformer extends Transformer implements TransformerInterface{
+class AccountsTransformer extends Transformer implements TransformerInterface {
 
-  public static function transform($record) {
-    return $record ? [
-      "id"                => (int)$record->id,
-      "name"              => $record->name,
-      "bank"              => $record->bank,
-      "sort_code"         => $record->acc_number,
-      "acc_number"        => $record->acc_number,
-      "open"              => (boolean)$record->open,
-      "opening_balance"   => 0.01 * $record->opening_balance,
-      "notes"             => $record->notes?:null ,
-      ] : null;
-  }
+    public static function transform($record) {
+        return $record ? [
+            "id" => (int)$record->id,
+            "name" => $record->name,
+            "bank" => $record->bank,
+            "sort_code" => $record->acc_number,
+            "acc_number" => $record->acc_number,
+            "open" => (boolean)$record->open,
+            "opening_balance" => 0.01 * $record->opening_balance,
+            "notes" => $record->notes ?: null,
+        ] : null;
+    }
 
-  public static function transformInput($record) {
-    if (isset($record["opening_balance"])) $record["opening_balance"] *= 100;
-    return $record;
-  }
+    public static function transformInput($record) {
+        if (isset($record["opening_balance"]))
+            $record["opening_balance"] *= 100;
+        return $record;
+    }
 
 
 }

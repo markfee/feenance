e56b7ff27c6d1604c96702fcdc46cfd0ed71f423
Mark Fee <git@markfee.com>
2015-04-11 11:10:11 +0100

    dropped trigger on update transaction

--- app/database/migrations/2014_08_20_182252_create_transactions_table.php
+++ app/database/migrations/2014_08_20_182252_create_transactions_table.php
@@ -3,118 +3,50 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateTransactionsTable extends Migration {
+class CreateTransactionsTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('transactions', function(Blueprint $table)
-		{
-      $table->engine = DB::connection()->getConfig("engine");
-			$table->increments('id');
-      $table->datetime('date');
-      $table->integer('amount');      // in pence, signed.
-      $table->integer('account_id')->unsigned();
-      $table->boolean('reconciled');
-      $table->integer('standing_order_id')->unsigned()->nullable();
-      $table->integer('payee_id')->unsigned()->nullable();
-      $table->integer('category_id')->unsigned()->nullable();
-			$table->string('notes')->nullable();
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->engine = DB::connection()->getConfig("engine");
+            $table->increments('id');
+            $table->datetime('date');
+            $table->integer('amount');      // in pence, signed.
+            $table->integer('account_id')->unsigned();
+            $table->boolean('reconciled');
+            $table->integer('standing_order_id')->unsigned()->nullable();
+            $table->integer('payee_id')->unsigned()->nullable();
+            $table->integer('category_id')->unsigned()->nullable();
+            $table->string('notes')->nullable();
 
-      $table->integer('bank_string_id')->unsigned()->nullable();
-      $table->integer('bank_balance')->nullable();
-			$table->timestamps();
+            $table->integer('bank_string_id')->unsigned()->nullable();
+            $table->integer('bank_balance')->nullable();
+            $table->timestamps();
 
-      $table->foreign('account_id')->references('id')->on('accounts');
-      $table->foreign('payee_id')->references('id')->on('payees');
-      $table->foreign('category_id')->references('id')->on('categories');
-      $table->foreign('standing_order_id')->references('id')->on('standing_orders');
-      $table->foreign('bank_string_id')->references('id')->on('bank_strings');
-      $table->index(['date', 'id']);
-		});
-
-      DB::unprepared('
-        CREATE TRIGGER transaction_update_balance AFTER UPDATE ON transactions FOR EACH ROW
-        BEGIN
-          IF @disable_transaction_triggers IS NULL THEN
-            IF NOT (NEW.account_id = OLD.account_id) THEN
-              CALL refresh_balances_for_account(OLD.account_id);
-              CALL refresh_balances_for_account(NEW.account_id);
-            ELSEIF NOT (NEW.amount = OLD.amount) OR NOT (NEW.date = OLD.date) THEN
-              SET @date = LEAST(NEW.date, OLD.date);
-              CALL refresh_balances_from_date(OLD.account_id, @date);
-            END IF;
-          END IF;
-        END;
-      ');
-
-      DB::unprepared('
-        CREATE TRIGGER transaction_insert_balance AFTER INSERT ON transactions FOR EACH ROW
-        BEGIN
-          IF @disable_transaction_triggers IS NULL THEN
-            CALL refresh_balances_from_date(NEW.account_id, NEW.date);
-          END IF;
-        END;
-      ');
-
-      DB::unprepared('
-        CREATE TRIGGER transaction_delete_balance AFTER DELETE ON transactions FOR EACH ROW
-        BEGIN
-          IF @disable_transaction_triggers IS NULL THEN
-            CALL refresh_balances_from_date(OLD.account_id, OLD.date);
-          END IF;
-        END;
-      ');
+            $table->foreign('account_id')->references('id')->on('accounts');
+            $table->foreign('payee_id')->references('id')->on('payees');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('standing_order_id')->references('id')->on('standing_orders');
+            $table->foreign('bank_string_id')->references('id')->on('bank_strings');
+            $table->index(['date', 'id']);
+        });
+    }
 
 
-
-      /*
-
-          DB::unprepared('
-            CREATE TRIGGER transaction_update_balance BEFORE UPDATE ON transactions FOR EACH ROW
-            BEGIN
-              IF NOT (NEW.amount <=> OLD.amount) THEN
-                SET @diff = NEW.amount - OLD.amount;
-                SET NEW.balance = OLD.balance + @diff;
-                UPDATE transactions SET balance = balance + @diff
-                  WHERE account_id = NEW.account_id
-                  AND (
-                          transactions.date > NEW.date
-                      OR (transactions.date <=> NEW.date AND transactions.id > NEW.id)
-                      );
-              END IF;
-            END;
-          ');
-          DB::unprepared('
-            CREATE TRIGGER transaction_insert_balance BEFORE INSERT ON transactions FOR EACH ROW
-            BEGIN
-              SET NEW.balance = NEW.amount + IFNULL(
-              (SELECT SUM(transactions.amount) FROM transactions
-                WHERE account_id = NEW.account_id
-                      AND
-                      (
-                          transactions.date < NEW.date
-                      OR (transactions.date <=> NEW.date AND transactions.id < NEW.id)
-                      )
-                ), 0)
-                ;
-            END;
-          ');*/
-  }
-
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('transactions');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('transactions');
+    }
 
 }
--- app/database/migrations/2014_11_06_072255_create_TransactionStatuses_table.php
+++ app/database/migrations/2014_11_06_072255_create_TransactionStatuses_table.php
@@ -3,51 +3,53 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateTransactionStatusesTable extends Migration {
+class CreateTransactionStatusesTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('transaction_statuses', function(Blueprint $table)
-		{
-      $table->engine = DB::connection()->getConfig("engine");
-			$table->increments('id');
-      $table->string('name')->nullable();
-      $table->char('code', 4);
-			$table->timestamps();
-		});
-    DB::unprepared("INSERT transaction_statuses VALUES(1, 'Unreconciled',             'U',    Now(), Now());");
-    DB::unprepared("INSERT transaction_statuses VALUES(2, 'Reconciled',               'R',    Now(), Now());");
-    DB::unprepared("INSERT transaction_statuses VALUES(3, 'Expected standing order',  'ESO',  Now(), Now());");
-    DB::unprepared("INSERT transaction_statuses VALUES(4, 'Planned definite',         'PD',   Now(), Now());");
-    DB::unprepared("INSERT transaction_statuses VALUES(5, 'Planned possible',         'PP',   Now(), Now());");
-
-    Schema::table('transactions', function(Blueprint $table)
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
     {
-      $table->integer('status_id')->default(1)->unsigned();
-      $table->foreign('status_id')->references('id')->on('transaction_statuses');
-    });
+        Schema::create('transaction_statuses', function (Blueprint $table) {
+            $table->engine = DB::connection()->getConfig("engine");
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->char('code', 4);
+            $table->timestamps();
+        });
+/*
+        DB::unprepared("INSERT transaction_statuses VALUES(1, 'Unreconciled',             'U',    Now(), Now());");
+        DB::unprepared("INSERT transaction_statuses VALUES(2, 'Reconciled',               'R',    Now(), Now());");
+        DB::unprepared("INSERT transaction_statuses VALUES(3, 'Expected standing order',  'ESO',  Now(), Now());");
+        DB::unprepared("INSERT transaction_statuses VALUES(4, 'Planned definite',         'PD',   Now(), Now());");
+        DB::unprepared("INSERT transaction_statuses VALUES(5, 'Planned possible',         'PP',   Now(), Now());");
+*/
 
-	}
+
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->integer('status_id')->default(1)->unsigned();
+            $table->foreign('status_id')->references('id')->on('transaction_statuses');
+        });
+
+    }
 
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-    Schema::table('transactions', function($table) {
-      $table->dropForeign('transactions_status_id_foreign');
-      $table->dropColumn('status_id');
-    });
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transactions', function ($table) {
+            $table->dropForeign('transactions_status_id_foreign');
+            $table->dropColumn('status_id');
+        });
 
-    Schema::dropIfExists('transaction_statuses');
-	}
+        Schema::dropIfExists('transaction_statuses');
+    }
 
 }

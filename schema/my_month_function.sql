d6d34c1ce78dff84894ee8ead6374d1e24973f0e
Mark Fee <git@markfee.com>
2015-04-30 08:16:20 +0100

    refactored fn_my_month and added fn_my_year

--- schema/my_month_function.sql
+++ schema/my_month_function.sql
@@ -2,18 +2,27 @@ USE feenance;
 
 DROP FUNCTION IF EXISTS fn_my_month;
 
-CREATE FUNCTION fn_my_month(p_date DATE, p_cuttoff TINYINT)  RETURNS INT
+CREATE FUNCTION fn_my_month(p_date DATE)  RETURNS INT
   BEGIN
-
-    DECLARE v_month INT;
-
-    IF (DAY(p_date) < p_cuttoff)
-    THEN
-      SET v_month = MONTH(p_date) - 1;
+    SET @cuttoff = 23;
+    IF (DAY(p_date) >= @cuttoff) THEN
+      RETURN  MONTH(p_date);
+    ELSE IF (MONTH(p_date) = 1 ) THEN
+      RETURN 12;
     ELSE
-      SET v_month = MONTH(p_date);
+      RETURN MONTH(p_date) - 1;
     END IF;
+    END IF;
+END;
 
-    RETURN v_month;
+DROP FUNCTION IF EXISTS fn_my_year;
 
-  END
+CREATE FUNCTION fn_my_year(p_date DATE)  RETURNS INT
+  BEGIN
+    SET @cuttoff = 23;
+    IF MONTH(p_date) = 1 AND (DAY(p_date) < @cuttoff) THEN
+      RETURN YEAR(p_date) - 1;
+    ELSE
+      RETURN  YEAR(p_date);
+    END IF;
+  END;
--- schema/v_transactions_by_month.sql
+++ schema/v_transactions_by_month.sql
@@ -2,16 +2,16 @@ use feenance;
 DROP VIEW IF EXISTS v_transactions_by_month;
 CREATE VIEW v_transactions_by_month AS
   SELECT
-    YEAR(transaction.date) AS year,
-    MONTH(transaction.date) AS month,
-    12 * YEAR(transaction.date) + MONTH(transaction.date) month_num,
+    FN_MY_YEAR(transaction.date) AS year,
+    FN_MY_MONTH(transaction.date) AS month,
+    12 * FN_MY_YEAR(transaction.date) + FN_MY_MONTH(transaction.date) month_num,
     SUM(transaction.credit) credit,
     SUM(transaction.debit) debit,
     SUM(transaction.movement) movement
   FROM
     v_non_transfers transaction
   GROUP BY year , month
-  ORDER BY year , month;
+  ORDER BY year DESC, month DESC;
 
 SELECT
     *

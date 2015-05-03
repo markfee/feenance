de7368c5f846ac668dcb888832963bb17b28edca
Mark Fee <git@markfee.com>
2015-05-03 08:50:11 +0100

    Transaction and Transformer use currency Converter

--- app/misc/transformer/TransactionTransformer.php
+++ app/misc/transformer/TransactionTransformer.php
@@ -1,24 +1,21 @@
 <?php namespace Feenance\Misc\Transformers;
 
+use Feenance\services\Currency\Currency;
 use Markfee\Responder\Transformer;
 use Markfee\Responder\TransformerInterface;
 
 class TransactionTransformer extends Transformer implements TransformerInterface
 {
-
-    private function transformAmount($account, $amount, $transfer)
-    {
-        return $account ? ["account_id" => $account, "amount" => 0.01 * $amount, "transfer_id" => $transfer] : null;
-    }
-
     public static function transform($record)
     {
+        $currencyConverter = Currency::createMainConverter($record->currency_code);
+
         return [
             "id" => (int)$record->id,
             "date" => $record->date->toISO8601String(),
-            "amount" => 0.01 * $record->amount,
+            "amount" => $currencyConverter->convert($record->amount),
             "account_id" => $record->account_id,
-            "balance" => $record->balance ? 0.01 * $record->balance->balance : null,
+            "balance" => $record->balance ? $currencyConverter->convert($record->balance->balance) : null,
             "reconciled" => $record->reconciled,
             "status" => $record->status ? $record->status->code : null,
             "payee_id" => $record->payee_id,
@@ -38,27 +35,32 @@ class TransactionTransformer extends Transformer implements TransformerInterface
                 ]
                 : null,
 
-            "bank_balance" => $record->bank_balance ? 0.01 * $record->bank_balance : null,
+            "bank_balance" => $currencyConverter->convert($record->bank_balance),
             "bank_string_id" => $record->bank_string_id ? (int)$record->bank_string_id : null,
             "bank_string" => $record->bankString ? $record->bankString->name : null,
             "payee" => $record->payee_id ? PayeeTransformer::transform($record->payee) : null,
             "category" => $record->category_id ? CategoryTransformer::transform($record->category) : null,
             "batch_id" => $record->batch_id,
+            "currency_code" => $record->currency_code,
         ];
     }
 
     public static function transformInput($record)
     {
+        $currencyConverter = Currency::createSubConverter($record->currency_code);
+
         $record["transformed"] = true; // Stop transforming input twice;
-        if (isset($record["amount"])) $record["amount"] *= 100;
+        if (isset($record["amount"])) {
+            $record["amount"] =  $currencyConverter->convert($record["amount"]);
+        }
         if (isset($record["date"])) {
             $record["date"] = substr($record["date"], 0, 10);
         }
-        if ( isset($record["balance"]) && is_numeric($record["balance"]) ) {
+        if (isset($record["balance"]) && is_numeric($record["balance"])) {
             $record["bank_balance"] = $record["balance"];
         }
-        if ( isset($record["bank_balance"]) && is_numeric($record["bank_balance"]) ) {
-            $record["bank_balance"] *= 100;
+        if (isset($record["bank_balance"]) && is_numeric($record["bank_balance"])) {
+            $record["bank_balance"] =  $currencyConverter->convert($record["bank_balance"]);
         }
         return $record;
     }
--- app/models/eloquent/Transaction.php
+++ app/models/eloquent/Transaction.php
@@ -3,89 +3,105 @@
 use DB;
 use Eloquent;
 
-class Transaction extends Eloquent {
-  protected $fillable = [
-      "date",
-      "amount",
-      "account_id",
-      "reconciled",
-      "payee_id",
-      "category_id",
-      "notes",
-      "bank_string_id",
-      "batch_id",
-      "bank_balance"
-  ];
+class Transaction extends Eloquent
+{
+    protected $fillable = [
+        "date",
+        "amount",
+        "account_id",
+        "reconciled",
+        "payee_id",
+        "category_id",
+        "notes",
+        "bank_string_id",
+        "batch_id",
+        "bank_balance"
+    ];
 
-  protected $dates = ["date"];
-  static public $rules = [
-    "date" => "required|date"
-    , "amount" => "required|integer|not_in:0"
-    , "account_id" => "required|integer"
-  ];
+    protected $dates = ["date"];
+    static public $rules = [
+        "date" => "required|date"
+        , "amount" => "required|integer|not_in:0"
+        , "account_id" => "required|integer"
+    ];
 
-  public static function startBulk() {
-    if (DB::connection()->getDriverName() == "sqlite")
-      return; // This won't work and isn't necessary with SQLITE
-    DB::unprepared('SET @disable_transaction_triggers = 1;');
-  }
 
-  public static function finishBulk($refresh = false) {
-    if (DB::connection()->getDriverName() == "sqlite")
-      return; // This won't work and isn't necessary with SQLITE
-    DB::unprepared('SET @disable_transaction_triggers = NULL;');
-    if ($refresh) {
-      DB::unprepared('call refresh_balances();');
-      Transaction::autoReconcile();
+    public static function startBulk()
+    {
+        if (DB::connection()->getDriverName() == "sqlite")
+            return; // This won't work and isn't necessary with SQLITE
+        DB::unprepared('SET @disable_transaction_triggers = 1;');
     }
-  }
 
-  /**
-   * Set Reconciled flag where balance matches bank balance.
-   * TODO: this function should also unreconcile reconciled transactions that don't match
-   */
-  public static function autoReconcile() {
-    DB::unprepared("
+    public static function finishBulk($refresh = false)
+    {
+        if (DB::connection()->getDriverName() == "sqlite")
+            return; // This won't work and isn't necessary with SQLITE
+        DB::unprepared('SET @disable_transaction_triggers = NULL;');
+        if ($refresh) {
+            DB::unprepared('call refresh_balances();');
+            Transaction::autoReconcile();
+        }
+    }
+
+    /**
+     * Set Reconciled flag where balance matches bank balance.
+     * TODO: this function should also unreconcile reconciled transactions that don't match
+     */
+    public static function autoReconcile()
+    {
+        DB::unprepared("
       UPDATE transactions JOIN balances
       ON balances.transaction_id = transactions.id AND transactions.bank_balance = balances.balance
       SET transactions.reconciled = 1,
        transactions.status_id = 2
        WHERE transactions.reconciled = 0 OR transactions.status_id != 2;"
-    );
-  }
+        );
+    }
 
-  public function balance() {
-    return $this->hasOne('Feenance\models\eloquent\Balance');
-  }
+    public function getCurrencyCodeAttribute($value) {
+        return "{$value}_pence";
+    }
 
-  public function destination() {
-    // If this is the source the transfer->destination is the destination
-    return $this->hasOne('Feenance\models\eloquent\Transfer', 'source');
-  }
+    public function balance()
+    {
+        return $this->hasOne('Feenance\models\eloquent\Balance');
+    }
 
-  public function source() {
-    // If this is the destination the transfer->source is the source
-    return $this->hasOne('Feenance\models\eloquent\Transfer', 'destination');
-  }
+    public function destination()
+    {
+        // If this is the source the transfer->destination is the destination
+        return $this->hasOne('Feenance\models\eloquent\Transfer', 'source');
+    }
 
-  public function bankString() {
-    // If this is the destination the transfer->source is the source
-    return $this->hasOne('Feenance\models\eloquent\BankString', "id", "bank_string_id");
-  }
+    public function source()
+    {
+        // If this is the destination the transfer->source is the source
+        return $this->hasOne('Feenance\models\eloquent\Transfer', 'destination');
+    }
 
-  public function payee() {
-    // If this is the destination the transfer->source is the source
-    return $this->hasOne('Feenance\models\eloquent\Payee', "id", "payee_id");
-  }
+    public function bankString()
+    {
+        // If this is the destination the transfer->source is the source
+        return $this->hasOne('Feenance\models\eloquent\BankString', "id", "bank_string_id");
+    }
 
-  public function category() {
-    // If this is the destination the transfer->source is the source
-    return $this->hasOne('Feenance\models\eloquent\Category', "id", "category_id");
-  }
+    public function payee()
+    {
+        // If this is the destination the transfer->source is the source
+        return $this->hasOne('Feenance\models\eloquent\Payee', "id", "payee_id");
+    }
 
-  public function status() {
-    // If this is the destination the transfer->source is the source
-    return $this->hasOne('Feenance\models\eloquent\TransactionStatus', "id", "status_id");
-  }
+    public function category()
+    {
+        // If this is the destination the transfer->source is the source
+        return $this->hasOne('Feenance\models\eloquent\Category', "id", "category_id");
+    }
+
+    public function status()
+    {
+        // If this is the destination the transfer->source is the source
+        return $this->hasOne('Feenance\models\eloquent\TransactionStatus', "id", "status_id");
+    }
 
 }

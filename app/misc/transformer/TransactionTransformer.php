08e64695e382d3e73e73028abeb499ae798bbba8
Mark Fee <git@markfee.com>
2015-05-03 09:11:51 +0100

    Transaction transformer sets default currency

--- app/misc/transformer/TransactionTransformer.php
+++ app/misc/transformer/TransactionTransformer.php
@@ -41,13 +41,14 @@ class TransactionTransformer extends Transformer implements TransformerInterface
             "payee" => $record->payee_id ? PayeeTransformer::transform($record->payee) : null,
             "category" => $record->category_id ? CategoryTransformer::transform($record->category) : null,
             "batch_id" => $record->batch_id,
-            "currency_code" => $record->currency_code,
+            "currency_code" => Currency::get_main_unit($record->currency_code),
         ];
     }
 
     public static function transformInput($record)
     {
-        $currencyConverter = Currency::createSubConverter($record->currency_code);
+        $currencyCode = isset($record["currency_code"]) ? $record["currency_code"] : null;
+        $currencyConverter = Currency::createSubConverter($currencyCode);
 
         $record["transformed"] = true; // Stop transforming input twice;
         if (isset($record["amount"])) {
@@ -62,6 +63,7 @@ class TransactionTransformer extends Transformer implements TransformerInterface
         if (isset($record["bank_balance"]) && is_numeric($record["bank_balance"])) {
             $record["bank_balance"] =  $currencyConverter->convert($record["bank_balance"]);
         }
+        $record["currency_code"] = Currency::get_sub_unit($currencyCode);
         return $record;
     }
 }

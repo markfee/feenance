924ab325a05efd68b645c78d85e6808ffd614708
Mark Fee <git@markfee.com>
2015-05-10 09:25:40 +0100

    MyCarbon can fuzzy match dates

--- app/misc/MyCarbon.php
+++ app/misc/MyCarbon.php
@@ -6,6 +6,106 @@ use \Carbon\Carbon;
 
 class MyCarbon extends Carbon
 {
+    const YEAR_PATTERN = '/^[0-9]{4}$/';
+    private static $monthNames = [
+        "January"     => 1,
+        "February"    => 2,
+        "March"       => 3,
+        "April"       => 4,
+        "May"         => 5,
+        "June"        => 6,
+        "July"        => 7,
+        "August"      => 8,
+        "September"   => 9,
+        "October"     => 10,
+        "November"    => 11,
+        "December"    => 12,
+        "Jan"         => 1,
+        "Feb"         => 2,
+        "Mar"         => 3,
+        "Apr"         => 4,
+        "May"         => 5,
+        "Jun"         => 6,
+        "Jul"         => 7,
+        "Aug"         => 8,
+        "Sep"         => 9,
+        "Oct"         => 10,
+        "Nov"         => 11,
+        "Dec"         => 12
+    ];
+
+    private static $dayNames = [
+        "Sunday"    =>  0,
+        "Monday"    =>  1,
+        "Tuesday"   =>  2,
+        "Wednesday" =>  3,
+        "Thursday"  =>  4,
+        "Friday"    =>  5,
+        "Saturday"  =>  6,
+        "Sun"       =>  0,
+        "Mon"       =>  1,
+        "Tue"       =>  2,
+        "Wed"       =>  3,
+        "Thu"       =>  4,
+        "Fri"       =>  5,
+        "Sat"       =>  6
+    ];
+
+    public function matchesMonth($string)
+    {
+        return (
+            !empty(static::$monthNames[$string])
+            &&  (static::$monthNames[$string] == $this->month)
+        );
+    }
+
+    public function matchesDay($string)
+    {
+        return (
+            !empty(static::$dayNames[$string])
+            &&  (static::$dayNames[$string] == $this->dayOfWeek)
+        );
+    }
+
+    public function matchesYear($string)
+    {
+        return ( preg_match(self::YEAR_PATTERN, $string) && (((int) $string) == $this->year) );
+    }
+
+    /**
+     * match that will return true if the date matches a year, dayname or monthname
+     * && is used to split strings that must ALL match
+     * || is used to split strings that must match at least one
+     * && takes precedence over ||
+     * TODO: allow for brackets
+     * @param $stringList
+     * @return bool
+     */
+    public function fuzzyMatch($stringList)
+    {
+        $strings = explode("&&", $stringList);
+        $failed = false;
+        foreach($strings as $string) {
+            $string = trim($string);
+            if ( ! $this->fuzzyMatchOneOf($string)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function fuzzyMatchOneOf($stringList)
+    {
+        $strings = explode("||", $stringList);
+        foreach($strings as $string) {
+            $string = trim($string);
+            if ($this->matchesDay($string) || $this->matchesMonth($string)  || $this->matchesYear($string)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public function earliest($date)
     {
         return empty($date) || ($this < $date) ? $this : clone($date);
--- app/tests/unit/MyCarbonTest.php
+++ app/tests/unit/MyCarbonTest.php
@@ -1,55 +1,104 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: mark
- * Date: 22/10/14
- * Time: 07:03
- */
+<?php namespace Feenance\tests\unit;
 
-namespace Feenance\tests\unit;
 use Feenance\tests\TestCase;
 use \Markfee\MyCarbon;
 
 
-class MyCarbonTest extends TestCase {
+class MyCarbonTest extends TestCase
+{
 
-  public function test_previous_day() {
-    $date         = MyCarbon::create(2014, 8, 1, 0)->previousWorkingDay();
-    $expectedDate = MyCarbon::create(2014, 7, 31, 0);
+    public function test_previous_day()
+    {
+        $date = MyCarbon::create(2014, 8, 1, 0)->previousWorkingDay();
+        $expectedDate = MyCarbon::create(2014, 7, 31, 0);
 
-    $this->assertTrue(0 === $date->diff($expectedDate)->days);
-  }
+        $this->assertTrue(0 === $date->diff($expectedDate)->days);
+    }
 
-    public function test_many_previous_days() {
-    $date = MyCarbon::create(2014, 8, 1, 0);
-    for ($i=0; $i < 12; $i++) {
-      $expectedDate = clone($date);
-      $expectedDate->previousWorkingDay();
+    public function test_many_previous_days()
+    {
+        $date = MyCarbon::create(2014, 8, 1, 0);
+        for ($i = 0; $i < 12; $i++) {
+            $expectedDate = clone($date);
+            $expectedDate->previousWorkingDay();
 
 //      print "\n" . $date . "(". $date->format("D") .  ") --- " . $expectedDate . "(". $expectedDate->format("D") .")";
 
-      $this->assertTrue($date->diff($expectedDate)->days > 0, "Are the days different?");
+            $this->assertTrue($date->diff($expectedDate)->days > 0, "Are the days different?");
 
-      // Test that days are less than or equal to 5 days apart (in the case where there are two subsequent bank holidays next to a weekend)
-      $this->assertTrue($date->diff($expectedDate)->days <= 5, "Are the days less than or equal to five apart?");
+            // Test that days are less than or equal to 5 days apart (in the case where there are two subsequent bank holidays next to a weekend)
+            $this->assertTrue($date->diff($expectedDate)->days <= 5, "Are the days less than or equal to five apart?");
 
-      $this->assertEquals(true, $expectedDate->isWeekday(), "Is Expected Day a weekday?");
+            $this->assertEquals(true, $expectedDate->isWeekday(), "Is Expected Day a weekday?");
 
-      $date->addMonth();
+            $date->addMonth();
+        }
     }
-  }
 
-    public function test_earliest() {
+    public function test_earliest()
+    {
         $date1 = new MyCarbon("2015-08-01");
         $date2 = new MyCarbon("2015-08-10");
         $date3 = new MyCarbon("2016-08-10");
 
-        $this->assertTrue( ($date1->earliest($date2)->isSameDay($date1)), "Expected {$date1} to be the earliest") ;
-        $this->assertTrue( ($date2->earliest($date1)->isSameDay($date1)), "Expected {$date1} to be the earliest") ;
+        $this->assertTrue(($date1->earliest($date2)->isSameDay($date1)), "Expected {$date1} to be the earliest");
+        $this->assertTrue(($date2->earliest($date1)->isSameDay($date1)), "Expected {$date1} to be the earliest");
 
-        $this->assertTrue( ($date3->earliest($date2)->isSameDay($date2)), "Expected {$date2} to be the earliest") ;
+        $this->assertTrue(($date3->earliest($date2)->isSameDay($date2)), "Expected {$date2} to be the earliest");
 
-        $this->assertTrue( ($date3->earliest($date1)->isSameDay($date1)), "Expected {$date1} to be the earliest") ;
+        $this->assertTrue(($date3->earliest($date1)->isSameDay($date1)), "Expected {$date1} to be the earliest");
 
     }
-};
+
+    public function assertFuzzyMatch($dateString, $fuzzyMatch, $expectedResult)
+    {
+        $date = new MyCarbon($dateString);
+        $not = $expectedResult ? ' ' : ' not ';
+        $this->assertTrue(
+            $date->fuzzyMatch($fuzzyMatch) === $expectedResult,
+            "Fuzzy Match Failed: Expected {$dateString} to{$not}match {$fuzzyMatch}");
+
+    }
+
+    public function test_fuzzy_match()
+    {
+        $this->assertFuzzyMatch("2015-01-15", "Jan", true);
+        $this->assertFuzzyMatch("2015-01-15", "Feb", false);
+        $this->assertFuzzyMatch("2015-01-15", "2015", true);
+        $this->assertFuzzyMatch("2015-01-15", "1915", false);
+        $this->assertFuzzyMatch("2015-01-15", "2014", false);
+        $this->assertFuzzyMatch("2015-01-15", "2016", false);
+        $this->assertFuzzyMatch("2015-01-15", "Feb", false);
+
+        $this->assertFuzzyMatch("2015-01-15", "crap", false);
+        $this->assertFuzzyMatch("2015-01-15", "Mon", false);
+        $this->assertFuzzyMatch("2015-01-15", "Tue", false);
+        $this->assertFuzzyMatch("2015-01-15", "Wed", false);
+        $this->assertFuzzyMatch("2015-01-15", "Thu", true);
+        $this->assertFuzzyMatch("2015-01-15", "Fri", false);
+        $this->assertFuzzyMatch("2015-01-15", "Sat", false);
+        $this->assertFuzzyMatch("2015-01-15", "Sun", false);
+
+        $this->assertFuzzyMatch("2015-01-15", "Jan||Feb", true);
+        $this->assertFuzzyMatch("2015-01-15", "Mar||Feb", false);
+        $this->assertFuzzyMatch("2015-01-15", "Thu||Feb", true);
+
+        $this->assertFuzzyMatch("2015-01-15", "2014&&Thu||Feb", false);
+        $this->assertFuzzyMatch("2015-01-15", "2015&&Thu||Feb", true);
+        $this->assertFuzzyMatch("2015-01-15", "2015&&Thu&&Jan", true);
+        $this->assertFuzzyMatch("2015-01-15", "2015&&Thu&&Feb", false);
+
+        $this->assertFuzzyMatch("2014-01-15", "2014||2015 && Wed||Thu||Fri && Jan||Feb", true);
+        $this->assertFuzzyMatch("2015-01-15", "2014||2015 && Wed||Thu||Fri && Jan||Feb", true);
+        $this->assertFuzzyMatch("2016-01-15", "2014||2015 && Wed||Thu||Fri && Jan||Feb", false);
+
+        $this->assertFuzzyMatch("2016-01-15", "Feb||Mar", false);
+        $this->assertFuzzyMatch("2016-02-15", "Feb||Mar", true);
+        $this->assertFuzzyMatch("2016-03-15", "Feb||Mar", true);
+        $this->assertFuzzyMatch("2016-04-15", "Feb||Mar", false);
+
+    }
+
+}
+
+;

6f54b9dea6d935d6dbc0a21c76f3d5763c9d66d8
Mark Fee <git@markfee.com>
2015-07-11 08:56:42 +0100

    Altered v_non_transfers sql view to also exclude transfers to loans as it did before, now that _non_transfers_with_loans exists.

--- schema/v_nontransfers.sql
+++ schema/v_nontransfers.sql
@@ -1,8 +1,6 @@
 use feenance;
 
 DROP VIEW IF EXISTS v_non_transfers;
-DROP VIEW IF EXISTS v_non_transfers_view;
-DROP VIEW IF EXISTS v_non_transfers_union;
 
 CREATE VIEW v_non_transfers AS
   SELECT  transaction.*
@@ -12,5 +10,6 @@ CREATE VIEW v_non_transfers AS
     LEFT JOIN v_transfers destination
       ON destination.destination_id   = transaction.id
   WHERE (source.id IS NULL AND destination.id IS NULL)
-        OR source.to_loan = 1
 ;
+
+SELECT * FROM v_non_transfers
